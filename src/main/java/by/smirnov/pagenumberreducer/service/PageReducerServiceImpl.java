a59d7456c1ac08d3940206d7a14fae678881651e
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2023-01-29 02:32:41 +0300

    Reduce method body added, fits main aims (26% tests not passed, needs optimization)

--- src/main/java/by/smirnov/pagenumberreducer/service/PageReducerServiceImpl.java
+++ src/main/java/by/smirnov/pagenumberreducer/service/PageReducerServiceImpl.java
@@ -2,12 +2,63 @@ package by.smirnov.pagenumberreducer.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
 @Service
 public class PageReducerServiceImpl implements PageReducerService {
 
+    public static final String DELIMITER = ",";
+    public static final String REDUCE_FORMAT = "%d-%d";
+
     @Override
     public String reduce(String numbers) {
-        return null;
+        List<Integer> sorted = sort(numbers);
+        StringJoiner joiner = new StringJoiner(DELIMITER);
+        LinkedList<Integer> buffer = new LinkedList<>();
+        for (Integer number : sorted) {
+            if (buffer.isEmpty() || isNextInRange(buffer.getLast(), number)) {
+                buffer.add(number);
+            } else {
+                joiner.add(checkAndFormat(buffer));
+                buffer = new LinkedList<>();
+                buffer.add(number);
+            }
+        }
+        if (!buffer.isEmpty()) joiner.add(checkAndFormat(buffer));
+
+        return joiner.toString();
     }
 
+    private List<Integer> sort(String numbers) {
+        return Arrays
+                .stream(numbers.split(DELIMITER))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .sorted()
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private boolean isNextInRange(Integer previous, Integer number) {
+        return previous == number - 1;
+    }
+
+    private String checkAndFormat(LinkedList<Integer> buffer) {
+        StringJoiner formatted = new StringJoiner(DELIMITER);
+        if (buffer.size() > 2) {
+            formatted.add(
+                    String.format(
+                            REDUCE_FORMAT, buffer.getFirst(), buffer.getLast()
+                    ));
+        } else {
+            for (Integer integer : buffer) {
+                formatted.add(String.valueOf(integer));
+            }
+        }
+        return formatted.toString();
+    }
 }
