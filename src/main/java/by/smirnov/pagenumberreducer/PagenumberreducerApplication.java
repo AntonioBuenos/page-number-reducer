76f86f587af5a2fdc343f62635ab7dea28784897
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2023-01-28 17:10:16 +0300

    Initial commit

---
+++ src/main/java/by/smirnov/pagenumberreducer/PagenumberreducerApplication.java
@@ -0,0 +1,13 @@
+package by.smirnov.pagenumberreducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagenumberreducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagenumberreducerApplication.class, args);
+	}
+
+}
